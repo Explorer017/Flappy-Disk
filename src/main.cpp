035424ccc003fb79b10c0d020f7f8232b178fba9
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2021-03-15 21:28:44 +0000

    made gravity and jumping a thing

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,70 @@
 #include <raylib-cpp.hpp>
+#include <string>
+//#include <iostream>
+//using namespace std;
+
+int screenWidth = 854;
+int screenHeight = 480;
+
+double acceleration = 1;
+double timestep = 1;
+double time = 0;
+double velocity = 0;
+
+double grav(double y) {
+    time += timestep;
+    y += timestep * (velocity + timestep * acceleration / 2);
+    velocity += timestep * acceleration;
+    if (y <= 0) {
+        y = 0;
+    }
+    return y;
+}
+
+void jump(double y){
+    velocity = -15;
+}
 
 int main() {
     
     // Initialization
-    int screenWidth = 800;
-    int screenHeight = 450;
+
 
     raylib::Color textColor(LIGHTGRAY);
-    raylib::Window w(screenWidth, screenHeight, "Good evening");
+    raylib::Window w(screenWidth, screenHeight, "Flappy Disk [ALPHA 0.1.0]");
     
     SetTargetFPS(60);
+
+    int playerSize = 50;
+    double y = (screenHeight/2)-(playerSize/2);
+    bool gameBegin = false;
+
 
     // Main game loop
     while (!w.ShouldClose()) // Detect window close button or ESC key
     {
         // Update
-
-        // TODO: Update your variables here
+        if (gameBegin) {
+            y = grav(y);
+            if (IsKeyPressed(KEY_SPACE)) {jump(y);}
+        }
+        else {
+            DrawText("Press [SPACE] to start", 20, (screenHeight/2)+playerSize+10,30,RED);
+            if (IsKeyPressed(KEY_SPACE)) {gameBegin = true;
+                                          jump(y);}
+            
+        }
 
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        textColor.DrawText("Good evening!", 190, 200, 20);
-        textColor.DrawText("Would you like a cup of tea?", 190, 225, 20);
+        DrawText(("Y: " + std::to_string(y)).c_str(),0,0,20,GOLD);
+        DrawRectangle((screenWidth/2)-(playerSize/2),y,playerSize,playerSize,RED);
         EndDrawing();
+
+
     }
 
     return 0;
 }
+
