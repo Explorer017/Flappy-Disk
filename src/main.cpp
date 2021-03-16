52e53c8d1fcbc4d900e2d489aec0f08cf8203fc2
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2021-03-16 18:13:48 +0000

    made the things thing the do

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,23 @@
 int screenWidth = 854;
 int screenHeight = 480;
 
+double deathModifier = 10;
 double acceleration = 1;
 double timestep = 1;
 double time = 0;
 double velocity = 0;
+bool gameBegin = false;
+
+class pipe {
+    public:
+        int x;
+        int height;
+        pipe(){
+            y = screenHeight-height;
+        }
+    private:
+        int y;
+};
 
 double grav(double y) {
     time += timestep;
@@ -17,6 +30,11 @@ double grav(double y) {
     velocity += timestep * acceleration;
     if (y <= 0) {
         y = 0;
+    }
+    else if (y >= ((screenHeight-50)+deathModifier)){
+        y = screenHeight/2;
+        velocity = 0;
+        gameBegin = false;
     }
     return y;
 }
@@ -36,8 +54,9 @@ int main() {
     SetTargetFPS(60);
 
     int playerSize = 50;
+    int pipes_on_screen = 0;
     double y = (screenHeight/2)-(playerSize/2);
-    bool gameBegin = false;
+
 
 
     // Main game loop
@@ -45,6 +64,7 @@ int main() {
     {
         // Update
         if (gameBegin) {
+            // Gravity
             y = grav(y);
             if (IsKeyPressed(KEY_SPACE)) {jump(y);}
         }
@@ -59,6 +79,7 @@ int main() {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText(("Y: " + std::to_string(y)).c_str(),0,0,20,GOLD);
+        DrawText(("Velocity: " + std::to_string(velocity)).c_str(),0,25,20,GOLD);
         DrawRectangle((screenWidth/2)-(playerSize/2),y,playerSize,playerSize,RED);
         EndDrawing();
 
