18e331b1b7c9b400041138e567461d3ae7159265
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2021-03-18 16:47:37 +0000

    pipes are now a thing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <raylib-cpp.hpp>
 #include <string>
 #include <ctime>
+#include <stdlib.h>
 //#include <iostream>
 //using namespace std;
 
@@ -8,24 +9,26 @@ int screenWidth = 854;
 int screenHeight = 480;
 
 double deathModifier = 10;
-double acceleration = 0.65;
+double acceleration = 0.4;
 double timestep = 0.1;
 double Time= 0;
 double velocity = 0;
 double deltaTime;
 double oldTime = clock();
+int pipeSpeed = 25;
 
 bool gameBegin = false;
 
 class pipe {
     public:
-        int x;
+        double x;
         int height;
-        pipe(){
-            y = screenHeight-height;
-        }
-    private:
-        int y;
+        void drawPipe(){
+            DrawRectangle(x,0,100,(screenHeight-height)- 200,GREEN);
+            DrawRectangle(x,(screenHeight-height),100,height,GREEN);
+    }
+
+
 };
 
 double grav(double y) {
@@ -50,7 +53,7 @@ void UpdateTime(){
 }
 
 void jump(double y){
-    velocity = -15;
+    velocity = -10;
 }
 
 int main() {
@@ -61,12 +64,15 @@ int main() {
     raylib::Color textColor(LIGHTGRAY);
     raylib::Window w(screenWidth, screenHeight, "Flappy Disk [ALPHA 0.1.0]");
     
-    SetTargetFPS(60);
+    SetTargetFPS(75);
 
-    int playerSize = 50;
+    int playerSize = 35;
     int pipes_on_screen = 0;
     double y = (screenHeight/2)-(playerSize/2);
 
+    pipe Pipe;
+    Pipe.x = screenWidth;
+    Pipe.height = 200;
 
 
     // Main game loop
@@ -80,6 +86,10 @@ int main() {
             // Gravity
             y = grav(y);
             if (IsKeyPressed(KEY_SPACE)) {jump(y);}
+            if (Pipe.x < 0-Pipe.height){ Pipe.x = screenWidth;
+                                         Pipe.height = rand() % (screenHeight-300) + 100;}
+            Pipe.x = Pipe.x - (pipeSpeed * deltaTime / 100);
+            Pipe.drawPipe();
         }
         else {
             DrawText("Press [SPACE] to start", 20, (screenHeight/2)+playerSize+10,30,RED);
@@ -88,6 +98,8 @@ int main() {
             
         }
 
+        
+        
         // Draw
         BeginDrawing();
         ClearBackground(RAYWHITE);
@@ -95,6 +107,8 @@ int main() {
         DrawText(("Downwards Velocity: " + std::to_string(velocity)).c_str(),0,25,20,GOLD);
         DrawText(("Time: " + std::to_string(Time)).c_str(),0,50,20,GOLD);
         DrawText(("FPS: " + std::to_string(GetFPS())).c_str(),0,75,20,GOLD);
+        DrawText(("Last DeltaTime: " + std::to_string(deltaTime)).c_str(),0,100,20,GOLD);
+        DrawText(("PipeX: " + std::to_string(Pipe.x)).c_str(),0,125,20,GOLD);
         DrawRectangle((screenWidth/2)-(playerSize/2),y,playerSize,playerSize,RED);
         EndDrawing();
 
